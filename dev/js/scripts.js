f2b0fa4c9675feee83312047843f483dceb0863e
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-05-03 19:42:09 -0400

    done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,26 +1,10 @@
 import { gsap } from "gsap";
-
-//elements
-/*
-.red-box
-.blue-box
-.green-box
-
-rotation suffix and
-rotation:"180_short", transformOrigin:"0 0"
-*/
-
-const mainTL = gsap.timeline({delay:2});
-
-//gsap.to(".blue-box",{ duration:3, rotation:360, transformOrigin:"center", repeat:-1, ease:"none"});
-
-mainTL.to(".red-box",{duration:1, x:500, ease:"power1.out"}, "box")
-.to(".blue-box",{duration:1, x:500, ease:"power3.out"}, "box")
-.to(".green-box",{duration:1, x:500, ease:"back.inOut"}, "box")
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 
-;
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin)
 
-mainTL.timeScale(0.25);
+const mainTL = gsap.timeline();
 
-//console.log("tl duration= "+tldur);
+mainTL
