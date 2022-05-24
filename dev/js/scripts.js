e479a2b4ce4f08133ed818511dc18b56db75e03a
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-05-24 17:10:10 -0400

    cleaning up

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,96 +1,137 @@
-import { gsap } from "gsap";
-import { GSDevTools } from "gsap/GSDevTools";
+import {
+    gsap
+} from "gsap";
+import {
+    GSDevTools
+} from "gsap/GSDevTools";
 
 gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 let hearts = gsap.utils.toArray(".heart");
 
-gsap.set(hearts, { autoAlpha: 0, y:"+=15", scale:0.6, transformOrigin:"center bottom" });
-let PERC = { num: 0 };
+gsap.set(hearts, {
+    autoAlpha: 0,
+    y: "+=15",
+    scale: 0.6,
+    transformOrigin: "center bottom"
+});
+let PERC = {
+    num: 0
+};
 let PERC_num = document.querySelector("#percentage tspan");
 
 function setStage() {
-  let tl = gsap.timeline();
+    let tl = gsap.timeline();
 
-  tl.set("#coffeebottom", { y: "+=155", alpha: 0.99 })
-    .set("#coffeetop", { y: "+=155", alpha: 1 })
-    .set("#number", { y: "+=95", alpha: 1 });
+    tl.set("#coffeebottom", {
+            y: "+=155",
+            alpha: 0.99
+        })
+        .set("#coffeetop", {
+            y: "+=155",
+            alpha: 1
+        })
+        .set("#number", {
+            y: "+=95",
+            alpha: 1
+        });
 
-  return tl;
+    return tl;
 }
 
 function showCoffee() {
-  let tl = gsap.timeline({ onStart: moveSteam });
+    let tl = gsap.timeline({
+        onStart: moveSteam
+    });
 
-  tl.to(
-    "#coffeebottom",
-    { duration: 5, y: "-=155", alpha: 1, ease: "sine.out" },
-    "coffee"
-  )
-    .to(
-      "#coffeetop",
-      { duration: 5, y: "-=155", alpha: 1, ease: "sine.out" },
-      "coffee"
-    )
-    .to(
-      PERC,
-      {
-        duration: 5,
-        num: "+=100",
-        roundProps: "num",
-        onUpdate: percentHandler,
-        ease: "sine.out"
-      },
-      "coffee"
-    )
-    .to(
-      "#number",
-      { duration: 5, y: "-=140", ease: "sine.out", alpha: 1 },
-      "coffee"
-    );
+    tl.to(
+            "#coffeebottom", {
+                duration: 5,
+                y: "-=155",
+                alpha: 1,
+                ease: "sine.out"
+            },
+            "coffee"
+        )
+        .to(
+            "#coffeetop", {
+                duration: 5,
+                y: "-=155",
+                alpha: 1,
+                ease: "sine.out"
+            },
+            "coffee"
+        )
+        .to(
+            PERC, {
+                duration: 5,
+                num: "+=100",
+                roundProps: "num",
+                onUpdate: percentHandler,
+                ease: "sine.out"
+            },
+            "coffee"
+        )
+        .to(
+            "#number", {
+                duration: 5,
+                y: "-=140",
+                ease: "sine.out",
+                alpha: 1
+            },
+            "coffee"
+        );
 
-  return tl;
+    return tl;
 }
 
 function percentHandler() {
-  //console.log("percentHandler ran");
-  PERC_num.textContent = PERC.num;
+    //console.log("percentHandler ran");
+    PERC_num.textContent = PERC.num;
 }
 
 function moveSteam() {
-  let dur = 5;
-  let hearts = gsap.utils.toArray(".heart");
-  hearts.forEach((heart, i) => {
-    gsap
-      .timeline({ repeat: -1, delay: i * 0.5 })
-      .to(heart, {
-        duration: dur * 0.4,
-        autoAlpha: 1,
-        repeat: 1,
-        yoyo: true,
-        repeatDelay: dur * 0.15,
-        ease: "none"
-      })
-      .to(
-        heart,
+    let dur = 5;
+    let hearts = gsap.utils.toArray(".heart");
+    hearts.forEach((heart, i) => {
+        gsap
+            .timeline({
+                repeat: -1,
+                delay: i * 0.5
+            })
+            .to(heart, {
+                duration: dur * 0.4,
+                autoAlpha: 1,
+                repeat: 1,
+                yoyo: true,
+                repeatDelay: dur * 0.15,
+                ease: "none"
+            })
+            .to(
+                heart,
 
-        { duration: dur, y: "-=45", ease: "none", scale:1 },
-        0
-      );
-  });
-  // gsap.fromTo(
-  //   ".heart",
-  //   { alpha: 0, y: "+=45" },
-  //   {
-  //     duration: 5,
-  //     y: "-=45",
-  //     stagger: 0.5,
-  //     ease: "back.inOut",
-  //     repeat: -1,
-  //     alpha: 1
-  //   }
-  // );
+                {
+                    duration: dur,
+                    y: "-=45",
+                    ease: "none",
+                    scale: 1
+                },
+                0
+            );
+    });
+    // gsap.fromTo(
+    //   ".heart",
+    //   { alpha: 0, y: "+=45" },
+    //   {
+    //     duration: 5,
+    //     y: "-=45",
+    //     stagger: 0.5,
+    //     ease: "back.inOut",
+    //     repeat: -1,
+    //     alpha: 1
+    //   }
+    // );
 }
 
 mainTL.add(setStage()).add(showCoffee());
